b47df106042e9e6c39d2a25bb9f50d386524c0d5
Verilog_lab28 Nachname <lab28@pool08.ifm.uni-ulm.de>
2019-01-12 18:39:59 +0100

    Started changes to divider

--- sources/syn/divider.v
+++ sources/syn/divider.v
@@ -20,55 +20,73 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module divider(Dividend,Divisor,Res,Busy,Ready);
+module divider(clock, reset, Dividend_spd, Dividend_avg_spd, Divisor_spd, Divisor_avg_spd, In_sel, Res, Valid_out);
 
-    //bit width of the divider, design uses 12
-    parameter WIDTH = 12;
+    `define SPD 0
+    `define AVG_SPD 1
     //input and output ports.
-    input [WIDTH-1:0] Dividend;
-    input [WIDTH-1:0] Divisor;
-    output [WIDTH-1:0] Res;
-    output reg Busy, Ready;
+    input clock;
+    input reset;
+    input [11:0] Dividend_spd;
+    input [11:0] Dividend_avg_spd;
+    input [11:0] Divisor_spd;
+    input [11:0] Divisor_avg_spd;
+    input In_sel; 
+    output [1:0] Res;
+    output reg Valid_out;
     
     //internal variables    
-    reg [WIDTH-1:0] Res = 0;
-    reg [WIDTH-1:0] a1,b1;
-    reg [WIDTH:0] p1;   
-    integer i;
-
-    always@ (Dividend or Divisor)
+    reg [11:0] Res = 0;
+    
+    
+    
+    always@ (posedge clock)
     begin
-        //initialize the variables.
-        Busy = 1;
-        a1 = Dividend;
-        b1 = Divisor;
-        p1 = 0;
-        Ready = 0;
-        //check division by 0
-        if (b1 == 0) begin
+        if(reset == 1) begin
             Res = 0;
-            Ready = 1;
-            Busy = 0;
         end
         else begin
-        
-        //restoring division
-            for(i=0;i < WIDTH;i=i+1)    begin 
-                p1 = {p1[WIDTH-2:0],a1[WIDTH-1]};#1;
-                a1[WIDTH-1:1] = a1[WIDTH-2:0];#1;
-                p1 = p1-b1;#1;
-                if(p1[WIDTH] == 1)    begin
-                   a1[0] = 0;#1;
-                   p1 = p1 + b1;#1;
-                end
-                else
-                    a1[0] = 1;#1;
-                end
-             
-            Res = a1;
-            Ready = 1;
-            Busy = 0;   
+            
         end
-     end 
+    end
+    
+    
+//    reg [11:0] a1,b1;
+//    reg [12:0] p1;
+//    integer i;
+//    always@ (Dividend or Divisor)
+//    begin
+//        //initialize the variables.
+//        Busy = 1;
+//        a1 = Dividend;
+//        b1 = Divisor;
+//        p1 = 0;
+//        Ready = 0;
+//        //check division by 0
+//        if (b1 == 0) begin
+//            Res = 0;
+//            Ready = 1;
+//            Busy = 0;
+//        end
+//        else begin
+        
+//        //restoring division
+//            for(i=0;i < WIDTH;i=i+1)    begin 
+//                p1 = {p1[WIDTH-2:0],a1[WIDTH-1]};#1;
+//                a1[WIDTH-1:1] = a1[WIDTH-2:0];#1;
+//                p1 = p1-b1;#1;
+//                if(p1[WIDTH] == 1)    begin
+//                   a1[0] = 0;#1;
+//                   p1 = p1 + b1;#1;
+//                end
+//                else
+//                    a1[0] = 1;#1;
+//                end
+             
+//            Res = a1;
+//            Ready = 1;
+//            Busy = 0;   
+//        end
+//     end 
 
 endmodule
